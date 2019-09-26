515288ff9c39c36e0a65415c46437d47468eede1
Xavier Lau <c.estlavie@icloud.com>
2019-09-26 13:34:26 +0800

    update: v0.7.4-alpha

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ lazy_static! {
 	static ref APP: ArgMatches<'static> = App::new("darwinia-builder")
 		.author("Xavier Lau <c.estlavie@icloud.com>")
 		.about("build tool for substrate")
-		.version("0.7.3-alpha")
+		.version("0.7.4-alpha")
 		.arg(
 			Arg::with_name("host")
 				.help("The HOST to build")
